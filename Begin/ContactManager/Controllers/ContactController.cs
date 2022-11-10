6ce69a104443d6f7f5507a0b0b39f5f1bfbc4613
Leon Vucko <lvucko@etfos.hr>
2022-11-10 18:49:00 +0100

    task4

--- Begin/ContactManager/Controllers/ContactController.cs
+++ Begin/ContactManager/Controllers/ContactController.cs
@@ -1,25 +1,20 @@
 ﻿using ContactManager.Models;
 using System.Web.Http;
+using ContactManager.Services;
 
 namespace ContactManager.Controllers
 {
     public class ContactController : ApiController
     {
+        private ContactRepository contactRepository;
+
+        public ContactController()
+        {
+            this.contactRepository = new ContactRepository();
+        }
         public Contact[] Get()
         {
-            return new Contact[]
-               {
-        new Contact
-        {
-            Id = 1,
-            Name = "Glenn Block"
-        },
-        new Contact
-        {
-            Id = 2,
-            Name = "Dan Roth"
-        }
-               };
+            return contactRepository.GetAllContacts();
         }
     }
 }
