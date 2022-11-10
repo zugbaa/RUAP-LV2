e125ea8708fb273f2fdf82fe9ddfedf685764f07
Leon Vucko <lvucko@etfos.hr>
2022-11-10 18:35:39 +0100

    task2

--- Begin/ContactManager/Controllers/ContactController.cs
+++ Begin/ContactManager/Controllers/ContactController.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Http;
 
 namespace ContactManager.Controllers
 {
-    public class ContactController : Controller
+    public class ContactController : ApiController
     {
         public string[] Get()
         {
@@ -15,10 +11,6 @@ namespace ContactManager.Controllers
         "Hello",
         "World"
             };
-        }
-        public ActionResult Index()
-        {
-            return View();
         }
     }
 }
